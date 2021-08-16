407b5199b8c2f615a6c38e3e39048b4b32c506ec
Stanley Horwood <stanley.horwood@hotmail.fr>
2021-08-16 03:36:33 +0000

    ✨ Added `LandingAnnotation` component

---
+++ src/components/home/LandingAnnotation.vue
@@ -0,0 +1,19 @@
+<script lang="ts">
+export default {
+    props: {
+        label: String,
+        icon: String
+    }
+}
+</script>
+
+
+<template>
+<div class="flex items-top space-x-8">
+    <Icon fab large brand :name="icon"/>
+    <div>
+        <Title class="mb-3" small>{{label}}</Title>
+        <Text class="!max-w-xs" secondary><slot/></Text>
+    </div>
+</div>
+</template>
