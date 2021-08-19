dfe9e139fe6d5d72eeef6fa3ab20aa844988c8c8
Stanley Horwood <stanley.horwood@hotmail.fr>
2021-08-19 16:40:54 +0000

    🔥 Back to `JavaScript` for core server logic

--- src/main.js
+++ src/main.js
@@ -6,8 +6,8 @@ import { createRouter } from './router'
 // that creates a fresh app instance. If using Vuex, we'd also be creating a
 // fresh store here.
 export function createApp() {
-  const app = createSSRApp(App)
-  const router = createRouter()
-  app.use(router)
-  return { app, router }
+    const app = createSSRApp(App)
+    const router = createRouter()
+    app.use(router)
+    return { app, router }
 }
